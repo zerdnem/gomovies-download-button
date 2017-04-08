27855b2bf84a7c9c0ff0ecbd8aa939ccab28c07b
zerdnem <zerdnem@gmail.com>
2017-04-08 16:35:45 +0800

    add video source for openload

--- script.js
+++ script.js
@@ -20,13 +20,19 @@ function setBtn() {
     var watching = url.match(str);
     if (watching) {
         var src = '';
-        var videoTags = document.getElementsByTagName('video')
+        var el = '';
+        var videoTags = document.getElementsByTagName('video');
+        if (videoTags.length == 0) {
+            var iframeTag = document.getElementById('iframe-embed').getAttribute('src');
+            src = iframeTag;
+            el = "<a href='' id='download-btn' class='btn'><i class='fa fa-download'></i> <span>Download</span></a>";
+        }
         for( var i = 0; i < videoTags.length; i++ ){
             src = videoTags.item(i).currentSrc;
+            el = "<a href='' download id='download-btn' class='btn'><i class='fa fa-download'></i> <span>Download</span></a>";
         }
-        var el = "<a href='' download id='download-btn' class='btn'><i class='fa fa-download'></i> <span>Download</span></a>";
-        var parent = document.getElementById('bar-player');
-        parent.innerHTML = el + parent.innerHTML;
+        var parentEl = document.getElementById('bar-player');
+        parentEl.innerHTML = el + parentEl.innerHTML;
         var dl = document.getElementById('download-btn').setAttribute('href', src);
     }
 }
